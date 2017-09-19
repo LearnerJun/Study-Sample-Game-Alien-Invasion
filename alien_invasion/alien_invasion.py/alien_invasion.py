ec995260280e7352fed36c559df37a09a8fc7323
June <xwj.june@gmail.com>
2017-09-19 23:32:34 +1200

    First Update
    comment added & background color set

--- alien_invasion/alien_invasion.py/alien_invasion.py
+++ alien_invasion/alien_invasion.py/alien_invasion.py
@@ -3,15 +3,29 @@ import sys
 import pygame
 
 def run_game():
+	#initialize game and create a screen
 	pygame.init()
-	screen = pygame.display.set_mode((1200, 800))
-	pygame.display.set_caption("Alien Invasion")
+	screen = pygame.display.set_mode((1200, 800)) #size of screen
+												  #screen is a surface, 
+	pygame.display.set_caption("Alien Invasion")  #refresh surface while game start looping
 	
+	#set the background color
+	bg_color = (230, 230, 230)
+	
+	#loop for starting game
 	while True:
 		
+		#monitor events - keyboard and mouse 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
-				
+		
+		#update background color when events monitored
+		screen.fill(bg_color)
+		
+		#display recent screen which is kept updating
 		pygame.display.flip()
-run_game()
+		
+run_game() #start the game
+		
+		
