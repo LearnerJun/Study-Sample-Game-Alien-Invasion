a92548b33ea35438fa1365896930de098c7b505e
June <xwj.june@gmail.com>
2017-09-21 23:40:11 +1200

    Correct Ship,py

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -8,10 +8,11 @@ class Ship():
         #get the image of Ship
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
 
         #set the position of ship to the mid screen
-        self.rect = self.image.get_rect.centerx
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
     def blitme(self):
